a87d6640ef6166c28e201a0f6ec57f407577ee2f
Tobi Ore <54361683+tobi-ore@users.noreply.github.com>
2024-08-05 00:49:29 -0500

    Update download_checkpoint.sh

--- checkpoints/download_checkpoint.sh
+++ checkpoints/download_checkpoint.sh
@@ -1,3 +1,1 @@
-DATASET=$1 # Available datasets: lsun_bedroom, ffhq, lsun_cat, lsun_horse 
-
-wget https://storage.yandexcloud.net/yandex-research/ddpm-segmentation/models/ddpm_checkpoints/${DATASET}.pt -P checkpoints/ddpm/
+Put DDPM checkpoint in this folder or download pretrained from 
