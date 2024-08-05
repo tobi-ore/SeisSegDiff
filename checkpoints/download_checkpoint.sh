619d841b543e9d60b3ac20b3736991d698b6229d
Tobi Ore <54361683+tobi-ore@users.noreply.github.com>
2024-08-05 00:50:29 -0500

    Update download_checkpoint.sh

--- checkpoints/download_checkpoint.sh
+++ checkpoints/download_checkpoint.sh
@@ -1,1 +1,1 @@
-Put DDPM checkpoint in this folder or download pretrained from https://drive.google.com/file/d/1ZLlF2p20C4FwiuCuGZJJ4_LCGLx9Q37U/view?usp=sharing
+Put DDPM checkpoint in this folder or download pretrained from [Tobi_model] (https://drive.google.com/file/d/1ZLlF2p20C4FwiuCuGZJJ4_LCGLx9Q37U/view?usp=sharing)
